1134e15eb26efe428ef3a9cc4b6ed9be8d537e95
Jan Sucan <sucanjan@gmail.com>
2019-07-04 10:22:28 +0200

    ping: tests: Add ping6 output test with option -c3

--- sbin/ping/tests/ping_test.sh
+++ sbin/ping/tests/ping_test.sh
@@ -1,15 +1,18 @@
-atf_test_case opts_c3
-opts_c3_head() {
+atf_test_case ping_c3
+ping_c3_head() {
 	atf_set "descr" "Stop after receiving 3 ECHO_RESPONSE packets"
 }
-opts_c3_body() {
+ping_c3_body() {
 	atf_check -s exit:0 -o save:std.out -e empty \
 		  ping -c 3 localhost
-	check_ping_statistics std.out $(atf_get_srcdir)/opts_c3.out
+	check_ping_statistics std.out $(atf_get_srcdir)/ping_c3.out
+	atf_check -s exit:0 -o save:std.out -e empty \
+		  ping -6 -c 3 localhost
+	check_ping_statistics std.out $(atf_get_srcdir)/ping6_c3.out
 }
 
 atf_init_test_cases() {
-	atf_add_test_case opts_c3
+	atf_add_test_case ping_c3
 }
 
 check_ping_statistics() {
